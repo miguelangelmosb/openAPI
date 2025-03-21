1d1fec3e91c259d13fd7bafc1f02b617479d0a76
Miguel Angel Mosquera Balaguera <miguelangelmosb@gmail.com>
2025-03-21 02:09:36 -0500

    Se agrega el endpoint PATCH para remplazar ingredientes del sancocho

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.get('/sancocho', (req, res) => {
   app.post('/sancocho', (req, res) => {
     const { ingredientes } = req.body;
     if (!ingredientes || !Array.isArray(ingredientes)) {
-      return res.status(400).json({ message: 'Debe enviar una lista de ingredientes' });
+      return res.status(400).json({ message: 'Se debe enviar una lista con ingredientes' });
     }
     sancocho = { id: Date.now(), ingredientes };
     res.status(201).json({
@@ -33,10 +33,31 @@ app.get('/sancocho', (req, res) => {
     }
     sancocho.ingredientes.push(...ingredientes);
     res.status(200).json({
-      message: 'Se han agregado los ingredienes correctamente',
+      message: 'Se han agregado los ingredientes correctamente',
       data: sancocho.ingredientes
     });
   });
+
+  app.patch('/sancocho', (req, res) => {
+    const { ingredienteActual, ingredienteNuevo } = req.body;
+    if (!ingredienteActual || !ingredienteNuevo) {
+      return res.status(400).json({
+        message: 'Se debe indicar el ingrediente actual y el nuevo'
+      });
+    }
+    const index = sancocho.ingredientes.indexOf(ingredienteActual);
+    if (index === -1) {
+      return res.status(404).json({
+        message: `El ingrediente ${ingredienteActual} no se encuentra en el sancocho`
+      });
+    }
+    sancocho.ingredientes[index] = ingredienteNuevo;
+    res.status(200).json({
+      message: 'El ingrediente se reemplazó correctamente',
+      data: sancocho.ingredientes
+    });
+  });
+  
 
 const PORT = 3000;
 app.listen(PORT, () => {
