3f9260f91883bd1a014e50fcaadffa048511d271
Miguel Angel Mosquera Balaguera <miguelangelmosb@gmail.com>
2025-03-21 00:31:03 -0500

    Inicialización de la variable sancocho con un objeto que incluye un arreglo con sus respectivos ingredientes

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 const express = require('express');
 const app = express();
 app.use(express.json());
+
+let sancocho = {
+    id: 1,
+    ingredientes: ['carne' , 'pollo' , 'yuca', 'papa', 'ají', 'cilantro', 'ahuyama', 'mazorca']
+};
+
