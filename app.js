39de50f793e06273eff48cda4adbde7e98ef6102
Miguel Angel Mosquera Balaguera <miguelangelmosb@gmail.com>
2025-03-24 22:40:30 -0500

    Integración de Swagger UI para la documentación de la API

--- app.js
+++ app.js
@@ -4,3 +4,10 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
+
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+
+const swaggerDocument = YAML.load('./openapi.yaml');
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
